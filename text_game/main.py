1933298178d8b50e4af192a49c401cc372cc0387
Jurand76 <jurand@interia.pl>
2023-07-08 23:58:55 +0200

    fix: text game

--- text_game/main.py
+++ text_game/main.py
@@ -1,47 +1,76 @@
-def wybor():
+import random
+
+etapy = ['pokoj', 'okno', 'stol', 'waga', 'kule']
+
+def wybor(min, max):
     result = ''
     while result is not '0':
-        result = input('Twoj wybor (1-3 lub 0) :')
+        result = input(f'Twoj wybor ({min}-{max} lub 0) :')
         if result == '0':
             print('Dziekuje za gre!')
             exit()
-        if (result is not '1') and (result is not '2') and (result is not '3'):
-            print('Nie ma takiej opcji')
         else:
             return result
 
 
-def opis_pomieszczenia():
-    print("Znajdujesz się w pokoju, na środku pokoju jest stół, a na nim waga")
-    print("Waga ma dwie szalki. Jeśli na którejś z nich leży cięższy przedmiot - szalka idzie w dół.")
-    print("Obok wagi jest 9 tak samo wyglądających kul. Jedna z kul jest odrobinę cięższa od pozostałych.")
-    print("Znajdź najcięższą kulę.")
+def opis(nazwa_pliku):
+    f = open(nazwa_pliku)
+    print(f.read())
+    f.close()
 
+class Kule:
+    kule = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    def __init__(self):
+        ciezsza = random.randint(1,9)
+        self.kule[ciezsza] = 2
 
-class Kula:
-    def __init__(self, numer, waga):
-        self.numer = numer
-        self.waga = waga
+    def waga(self, numer):
+        return self.kule[numer]
 
 
 class Waga:
-    def __init__(self, szalka_lewa, szalka_prawa):
-        self.szalka_lewa = szalka_lewa
-        self.szalka_prawa = szalka_prawa
+    liczba_wazen = 0
 
+    def __init__(self):
+        self.szalka_lewa = 0
+        self.szalka_prawa = 0
+        self.liczba_lewa = 0
+        self.liczba_prawa = 0
     def wazenie(self):
+        if (self.szalka_lewa == 0) and (self.szalka_prawa == 0):
+            print("Szalki wagi są puste")
+            return 0
         if self.szalka_lewa < self.szalka_prawa:
+            self.liczba_wazen += 1
             return -1
         if self.szalka_lewa > self.szalka_prawa:
+            self.liczba_wazen += 1
             return 1
         if self.szalka_lewa == self.szalka_prawa:
+            self.liczba_wazen += 1
             return 0
 
     def doloz_z_lewej(self, ciezar):
         self.szalka_lewa = self.szalka_lewa + ciezar
+        self.liczba_lewa += 1
 
     def doloz_z_prawej(self, ciezar):
         self.szalka_prawa = self.szalka_prawa + ciezar
+        self.liczba_prawa += 1
+
+    def zabierz_z_lewej(self, ciezar):
+        if self.liczba_lewa > 0:
+            self.szalka_lewa = self.szalka_lewa - ciezar
+            self.liczba_lewa -= 1
+        else:
+            print("Nie ma nic na lewej szalce")
+
+    def zabierz_z_prawej(self, ciezar):
+        if self.liczba_prawa > 0:
+            self.szalka_prawa = self.szalka_prawa - ciezar
+            self.liczba_prawa -= 1
+        else:
+            print("Nie ma nic na prawej szalce")
 
     def zeruj_wage(self):
         self.szalka_lewa = 0
@@ -65,13 +94,68 @@ class Stol:
     def opisz_stol(self):
         print(f'Na stole jest następująca liczba kul: {self.liczba_kul}')
 
+def zmien_pozycje():
+    global poziom
+    print('Co chcesz zrobić?')
+    print('   1 - podejdź do okna')
+    print('   2 - podejdź do stołu')
+    print('   0 - zakończ grę')
+    key = wybor(1, 2)
+    if key == '1':
+        poziom = etapy[1]
+    if key == '2':
+        poziom = etapy[2]
+
+def jestes_w_pokoju():
+    global poziom
+    print('Jesteś w pokoju. Wybierz opcję:')
+    print('   1 - opis pomieszczenia')
+    print('   2 - zmień pozycję')
+    print('   0 - zakończ grę')
+    key = wybor(1, 2)
+    if key == '1':
+        opis('pokoj.txt')
+    if key == '2':
+        zmien_pozycje()
+
+def jestes_przy_oknie():
+    global poziom
+    print('Jesteś przy oknie. Wybierz opcję:')
+    print('   1 - opis przedmiotu')
+    print('   2 - użyj przedmiotu')
+    print('   3 - zmień pozycję')
+    print('   0 - zakończ grę')
+    key = wybor(1, 3)
+    if key == '1':
+        opis('okno.txt')
+    if key == '2':
+        print('Okno nie daje się otworzyć.')
+    if key == '3':
+        zmien_pozycje()
+
+def jestes_przy_stole():
+    global poziom
+    print('Jesteś przy stole. Wybierz opcję:')
+    print('   1 - opis przedmiotu')
+    print('   2 - użyj przedmiotu')
+    print('   3 - odejdź od stołu')
+    print('   0 - zakończ grę')
+    key = wybor(1, 3)
+    if key == '1':
+        opis('stol.txt')
+    if key == '2':
+        poziom = etapy[3]
+    if key == '3':
+        poziom = etapy[0]
+
+poziom = etapy[0]
 
 print('Witaj w grze')
-print('Wybierz opcję:')
-print('   1 - opis pomieszczenia')
-print('   2 - zabierz przedmiot')
-print('   3 - użyj przedmiot')
-print('   0 - zakończ grę')
 
-key = wybor()
-print(key)
+while True:
+    if poziom == etapy[0]:
+        jestes_w_pokoju()
+    if poziom == etapy[1]:
+        jestes_przy_oknie()
+    if poziom == etapy[2]:
+        jestes_przy_stole()
