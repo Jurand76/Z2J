94585d98fda4b95a1ae6489443e09ee5344bf5ed
Jurand76 <jurand@interia.pl>
2023-07-09 23:39:13 +0200

    fix: sudoku game

--- sudoku_game/main.py
+++ sudoku_game/main.py
@@ -1,5 +1,23 @@
 import random
 
+
+def generate_numbers(size):
+    table = []
+    for i in range(1, size * size + 1):
+        table.append(i)
+    return table
+
+
+def generate_one_sudoku(size):
+    numbers = generate_numbers(size)
+    sudoku_pow = size * size
+    sudoku = []
+    for i in range(1, sudoku_pow + 1):
+        random_number = random.randint(1, sudoku_pow + 1 - i)
+        sudoku.append(numbers[random_number - 1])
+        numbers.remove(sudoku[i - 1])
+    return sudoku
+
 def check_sudoku(table, size):
     sum_list = []
     sum_1 = 0
@@ -9,8 +27,8 @@ def check_sudoku(table, size):
 
     for a in range(0, size):
         for b in range(0, size):
-            sum_1 = sum_1 + sudoku[a * size + b]
-            sum_2 = sum_2 + sudoku[a + b * size]
+            sum_1 = sum_1 + table[a * size + b]
+            sum_2 = sum_2 + table[a + b * size]
         sum_list.append(sum_1)
         sum_list.append(sum_2)
         sum_1 = 0
@@ -22,33 +40,25 @@ def check_sudoku(table, size):
 
     return result
 
-
-def generate_numbers(size):
-    table = []
-    for i in range(1, size * size + 1):
-        table.append(i)
-    return table
-
-
-sudoku_str = input("Podaj szerokość planszy (2-20): ")
-sudoku_size = int(sudoku_str)
-
-sudoku_pow = sudoku_size * sudoku_size
-numbers = generate_numbers(sudoku_size)
-sudoku = []
-
-for i in range(1, sudoku_pow+1):
-    print(' ', end='')
-    random_number = random.randint(1, sudoku_pow+1-i)
-    sudoku.append(numbers[random_number-1])
-    print(sudoku[i - 1], ' ', end='')
-    if sudoku[i-1] < 10:
+def draw_board(table, size):
+    for i in range(1, size ** 2 + 1):
         print(' ', end='')
-    numbers.remove(sudoku[i - 1])
-    if i % sudoku_size == 0:
-        print()
+        print(table[i - 1], ' ', end='')
+        if table[i - 1] < 10:
+            print(' ', end='')
+        if i % size == 0:
+            print()
 
-if check_sudoku(sudoku, sudoku_size):
+input_str = input("Podaj szerokość planszy (2-20): ")
+board_size = int(input_str)
+input_str = input("Podaj ile wygenerować plansz(4/9/16/25): ")
+board_count = int(input_str)
+
+sudoku_board = generate_one_sudoku(board_size)
+draw_board(sudoku_board, board_size)
+
+
+if check_sudoku(sudoku_board, board_size):
     print("To jest poprawnie wykonane sudoku")
 else:
     print("To nie jest poprawnie wykonane sudoku")
