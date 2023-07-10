bbf67892411968c1b58601fdfcf9e429cdf90655
Jurand76 <jurand@interia.pl>
2023-07-11 00:40:14 +0200

    fix: Tetris game - background grid

--- tetris/main.py
+++ tetris/main.py
@@ -3,26 +3,26 @@ import math
 
 pg.init()
 
+def grid_background(scr, color, color_back, size):
+    for i in range(0, int(640/size)):
+        for j in range(0, int(1040/size)):
+            pg.draw.rect(scr, color, pg.Rect(i*size, j*size, size, size))
+            pg.draw.rect(scr, color_back, pg.Rect(i * size+1, j * size+1, size-2, size-2))
+
+
 
 FPS = 60
 screen = pg.display.set_mode((640, 1040))
 screen.fill((0, 0, 0))
-posx = 200
-posy = 200
-rot = 0
-klocek_szer = 40
-
-
 
 running = True
 while running:
     pg.time.Clock().tick(FPS)
-    screen.fill((0, 0, 0))
-
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             running = False
+            exit()
         if event.type == pg.KEYDOWN:
             if pg.key.get_pressed()[pg.K_s]:
                 print("S")
@@ -35,6 +35,8 @@ while running:
             if pg.key.get_pressed()[pg.K_SPACE]:
                 print("Space")
 
+    screen.fill((0, 0, 0))
+    grid_background(screen, (50, 50, 50), (20, 20, 20), 40)
     pg.display.update()
 
 
