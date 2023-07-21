3e5de2d501bc9c6a2468f05a8b13090ba8b28934
Jurand76 <jurand@interia.pl>
2023-07-22 01:04:36 +0200

    feat: sockets - getting time of server life

--- P202_sockets/server.py
+++ P202_sockets/server.py
@@ -1,8 +1,16 @@
 import json
 import socket
+from datetime import datetime
 
 HOST = "127.0.0.1"
 PORT = 65432
+start_time = datetime.now()
+print(start_time)
+
+def getServerTime():
+    global start_time
+    time_now = datetime.now()
+    return time_now - start_time
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -11,9 +19,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print(f"Connected by {addr}")
         while True:
-            data = conn.recv(1024)
+            data = conn.recv(1024).decode('utf8')
+            print(f"Received data: {data}")
+            if data == "uptime":
+                response = str(getServerTime())
+                conn.sendall(json.dumps(response).encode('utf8'))
             if not data:
                 break
-            conn.sendall(data)
+
 
 
