2217188f42d5f57ebf45350b560d9b2087eea72d
Jurand76 <jurand@interia.pl>
2023-07-24 22:38:45 +0200

    feat: server object making

--- P202_sockets/server.py
+++ P202_sockets/server.py
@@ -2,50 +2,53 @@ import json
 import socket
 from datetime import datetime
 
-HOST = "127.0.0.1"
-PORT = 65432
-start_time = datetime.now()
-VERSION = str(f"Server at host {HOST} working on port {PORT}. Software version 1.0. Server started {start_time}")
-print(start_time)
+
+class Server:
+    def __init__(self):
+        self.HOST = "127.0.0.1"
+        self.PORT = 65432
+        self.start_time = datetime.now()
+        self.VERSION = str(
+            f"Server at host {self.HOST} working on port {self.PORT}. Software version 1.0. Server started {self.start_time}")
+
+    def get_server_time(self):
+        time_now = datetime.now()
+        return time_now - self.start_time
+
+    def help_command(self):
+        help_data = {"uptime": "zwraca czas zycia serwera",
+                     "info": "zwraca numer wersji serwera, date jego utworzenia",
+                     "help": "zwraca liste dostepnych komend",
+                     "stop": "zatrzymuje jednoczesnie serwer i klienta"}
+        return help_data
+
+    def server_answer(self, connection, response):
+        connection.sendall(json.dumps(response).encode('utf8'))
+
+    def server_start(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.HOST, self.PORT))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(1024).decode('utf8')
+                    print(f"Received data: {data}")
+                    if data == "info":
+                        response = self.VERSION
+                        self.server_answer(conn, response)
+                    if data == "uptime":
+                        response = str(self.get_server_time())
+                        self.server_answer(conn, response)
+                    if data == "help":
+                        response = self.help_command()
+                        self.server_answer(conn, response)
+                    if data == "stop":
+                        response = "Server stopped by user"
+                        self.server_answer(conn, response)
+                        break
 
 
-def get_server_time():
-    global start_time
-    time_now = datetime.now()
-    return time_now - start_time
-
-
-def help_command():
-    help_data = {"uptime": "zwraca czas zycia serwera",
-                 "info": "zwraca numer wersji serwera, date jego utworzenia",
-                 "help": "zwraca liste dostepnych komend",
-                 "stop": "zatrzymuje jednoczesnie serwer i klienta"}
-    return help_data
-
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024).decode('utf8')
-            print(f"Received data: {data}")
-            if data == "info":
-                response = VERSION
-                conn.sendall(json.dumps(response).encode('utf8'))
-            if data == "uptime":
-                response = str(get_server_time())
-                conn.sendall(json.dumps(response).encode('utf8'))
-            if data == "help":
-                response = help_command()
-                conn.sendall(json.dumps(response).encode('utf8'))
-            if data == "stop":
-                response = "Server stopped by user"
-                conn.sendall(json.dumps(response).encode('utf8'))
-                break
-            if not data:
-                response = "Server stopped without data"
-                conn.sendall(json.dumps(response).encode('utf8'))
-                break
+server = Server()
+server.server_start()
